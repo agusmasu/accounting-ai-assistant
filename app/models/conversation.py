b77727e0517f67154ec7c0a5d69b5a3189d4797b
agustinmasuelli <agus.masu@gmail.com>
2025-04-22 00:14:15 -0300

    Adding uuid to the conversation model

--- app/models/conversation.py
+++ app/models/conversation.py
@@ -1,8 +1,9 @@
+import uuid
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
 class Conversation(SQLModel, table=True):
-    id: str | None = Field(default=None, primary_key=True)
+    id: str | None = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     created_at: datetime | None = Field(default=datetime.now())
     updated_at: datetime | None = Field(default=datetime.now())
     user_id: str | None = Field(default=None)

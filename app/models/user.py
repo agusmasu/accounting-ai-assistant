12e31bc8dc00d86ab20340415038fbe264f72dc4
agustinmasuelli <agus.masu@gmail.com>
2025-04-22 00:11:33 -0300

    Adding API key, and some improvements in the user table

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from sqlmodel import Field, SQLModel
+import uuid
 
 class User(SQLModel, table=True):
-    id: str | None = Field(default=None, primary_key=True)
+    id: str | None = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     name: str = Field(default=None)
     email: str = Field(default=None)
     user_token: str | None = Field(default=None)

30331120aae63006ac1983248edd7812e51547a6
agustinmasuelli <agus.masu@gmail.com>
2025-05-06 22:09:03 -0300

    Creating a class for the tools

--- app/services/ai.py
+++ app/services/ai.py
@@ -11,10 +11,11 @@ from langchain.schema import HumanMessage, AIMessage
 from langgraph.checkpoint.memory import MemorySaver
 from app.models.user import User
 from app.services.conversation import ConversationService
-from app.services.tools.invoice import create_invoice, create_invoice_test
+from app.services.tools.invoice import InvoiceToolsService, create_invoice, create_invoice_test
 from app.services.memory import MemoryService
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from app.services.tusfacturas import TusFacturasService
 from app.services.user import UserService
 from openai import OpenAI
 
@@ -68,7 +69,10 @@ class AIService:
         # )
 
         # Set up tools
-        self.tools = [create_invoice]
+
+        tusfacturas_service = TusFacturasService(self.user_service)
+        self.invoice_tools_service = InvoiceToolsService(tusfacturas_service)
+        self.tools = [self.invoice_tools_service.create_invoice]
         # self.tools = [create_invoice_test]
         
         # Initialize memory for conversation persistence
--- app/services/tools/invoice.py
+++ app/services/tools/invoice.py
@@ -8,30 +8,35 @@ import asyncio
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-@tool(name_or_callable="create_invoice_test", description="Create an invoice using the TusFacturas API")
-def create_invoice_test():
-    """Create an invoice using the TusFacturas API
-    
-    Returns:
-        dict: The invoice data
-    """
-    return "Invoice created wirh id 1"
+class InvoiceToolsService:
 
-@tool(name_or_callable="create_invoice", description="Create an invoice using the TusFacturas API")
-def create_invoice(invoice_data: InvoiceInputData, user_id: str):
-    """Create an invoice using the TusFacturas API
-    
-    Args:
-        invoice_data (Invoice): The invoice data to create
-        user_id (str): The user id of the user who is creating the invoice
+    def __init__(self, tusfacturas_service: TusFacturasService):
+        self.tusfacturas_service = tusfacturas_service
 
-    Returns:
-        dict: The invoice data
-    """
-    
-    # Print the invoice data
-    logger.info(f"Creating invoice with the following data: {invoice_data}")
+    @tool(name_or_callable="create_invoice_test", description="Create an invoice using the TusFacturas API")
+    def create_invoice_test():
+        """Create an invoice using the TusFacturas API
+        
+        Returns:
+            dict: The invoice data
+        """
+        return "Invoice created wirh id 1"
 
-    tusfacturas_service = TusFacturasService()
-    return asyncio.run(tusfacturas_service.generate_invoice(invoice_data, user_id))
+    @tool(name_or_callable="create_invoice", description="Create an invoice using the TusFacturas API")
+    def create_invoice(invoice_data: InvoiceInputData, user_id: str):
+        """Create an invoice using the TusFacturas API
+        
+        Args:
+            invoice_data (Invoice): The invoice data to create
+            user_id (str): The user id of the user who is creating the invoice
+
+        Returns:
+            dict: The invoice data
+        """
+        
+        # Print the invoice data
+        logger.info(f"Creating invoice with the following data: {invoice_data}")
+
+        tusfacturas_service = TusFacturasService()
+        return asyncio.run(tusfacturas_service.generate_invoice(invoice_data, user_id))
 
